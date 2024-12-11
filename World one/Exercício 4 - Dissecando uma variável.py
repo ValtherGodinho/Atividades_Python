c82f901c4d82698a5bde8e94a5e813975e5ada14
ValtherGodinho <valthergodinho68@hotmail.com>
2024-12-11 16:24:52 -0300

    Ex4

---
+++ World one/Exercício 4 - Dissecando uma variável.py
@@ -0,0 +1,13 @@
+a = input('Digite algo: ')
+print('O tipo primitivo desse valor é ', type(a))
+print('Só tem espaços? ', a.isspace())
+print('É um número? ', a.isnumeric() )
+print('É alfabetico?', a.isalpha() )
+print('É alfanumerico? ', a.isalnum() )
+print('Esta em maiúsculas?', a.isupper() )
+print('Esta em minúsculas?', a.islower() )
+print('Esta capitalizada?', a.istitle() )
+
+
+
+

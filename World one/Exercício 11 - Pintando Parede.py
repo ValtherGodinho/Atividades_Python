57d659c1cec71c5a961d17389c179f6f6113f359
ValtherGodinho <valthergodinho68@hotmail.com>
2024-12-16 10:21:50 -0300

    Ex. 10, 11, 12, 13, 14, 15.

---
+++ World one/Exercício 11 - Pintando Parede.py
@@ -0,0 +1,7 @@
+larg = float(input('Largura da parede: '))
+alt = float(input('Altura da parede: '))
+area = larg * alt
+print('Sua parede tem a dimensao de {} x {} e sua area e de {}m2.'.format(larg, alt,  area))
+tinta = area / 2 
+print('Para pintar essa parede, voce precisara de {}l de tinta'.format(tinta))
+
---
+++ World one/Exercício 15 - Aluguel de carro.py
@@ -0,0 +1,4 @@
+dias = int(input('Quantos dias alugandos?  '))
+Km = float(input('Quantos Km rodados? '))
+pago = (dias * 60) + (Km * 0.15)
+print('O total a pagar e de R${:.2f}'.format(pago))
